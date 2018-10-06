94fac3fb717f43bd6e69583d88fc2eb9418a9528
tolabey <tolamusti@gmail.com>
2018-10-06 19:32:04 +0300

    Version1

--- src/Utils/utils.js
+++ src/Utils/utils.js
@@ -3,7 +3,6 @@ import {action} from "../reduxStore/action";
 export const utils = {
   testUtil(dispatch) {
     return () => {
-    console.log("mustafa")
       dispatch(action("TEST", 1));
     }
   }
